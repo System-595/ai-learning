064eeed31f19e1e7b81b39526679a88870f471e8
Divyaansh Rai Gandhi <divyaanshraigandhi@gmail.com>
2024-12-19 07:16:22 +0530

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -2,14 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBiXP3zR7iEr6sSn1ZOU-ZMhLST_TFNTeU",
-    authDomain: "ai-learning-platform-63fb3.firebaseapp.com",
-    projectId: "ai-learning-platform-63fb3",
-    storageBucket: "ai-learning-platform-63fb3.firebasestorage.app",
-    messagingSenderId: "499234458814",
-    appId: "1:499234458814:web:477f0417c193c0b12e62ef",
-    measurementId: "G-VWS8466YTC"
-  };
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID",
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
